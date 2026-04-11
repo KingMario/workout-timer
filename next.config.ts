a698cba2d0e61357592aaa63ae1c263645f15b0a
Changyu Geng <gcyyq@hotmail.com>
2026-04-11 12:15:31 +0800

    feat: redirect root to basePath in development mode

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,19 @@ const nextConfig: NextConfig = {
   // site is served under /workout-timer
   basePath: '/workout-timer',
   assetPrefix: '/workout-timer/',
+  // Use redirects only in development for convenience
+  ...(process.env.NODE_ENV === 'development' && {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/workout-timer',
+          basePath: false,
+          permanent: false,
+        },
+      ];
+    },
+  }),
 };
 
 export default nextConfig;
